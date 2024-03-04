251fe927c038f93d5122df2d24cb70fb3188d753
BalaSivaSaiMegiReddyPadala <megireddy21@gmail.com>
2024-03-04 17:24:38 +0530

    Update code formatting and fix bug

---
+++ src/components/HangmanDrawing.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function HangmanDrawing() {
+  return (
+
+    <div style={{position:"relative"}}>
+                <div style={{height:"50px",width:"10px",background:"black",position:"absolute",right:"0px",top:"0px"}}/>
+
+        <div style={{height:"10px",width:"200px",background:"black",marginLeft:"120px"}}/>
+        
+
+        <div style={{height:"400px",width:"10px",background:"black",marginLeft:"120px"}}/>
+        <div style={{height:"10px",width:"250px",background:"black"}}/>
+    </div>
+  )
+}

0df294ae791250aa64a1961c0bf9f6a1ba8cbc28
BalaSivaSaiMegiReddyPadala <megireddy21@gmail.com>
2024-03-04 17:51:10 +0530

    Add HangmanDrawing component with head and body

--- src/components/HangmanDrawing.tsx
+++ src/components/HangmanDrawing.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
+
+const HEAD=(<div style={{height:"60px",width:"60px",borderRadius:"100%",position:"absolute",border:"10px solid black",top:"40px",right:"-25px"}}/>)
+
+const BODY=(<div style={{width:"10px",height:"100px",background:"black",position:"absolute",top:"95px",right:"0"}}/>)
+
 export default function HangmanDrawing() {
   return (
+    
 
     <div style={{position:"relative"}}>
+        {HEAD}
+        {BODY}
                 <div style={{height:"50px",width:"10px",background:"black",position:"absolute",right:"0px",top:"0px"}}/>
 
         <div style={{height:"10px",width:"200px",background:"black",marginLeft:"120px"}}/>
